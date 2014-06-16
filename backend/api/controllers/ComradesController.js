5a862b1c1cde3f98ff601d22b5f9bcaee198ff81
Aaron Russell <aaron@teknologenie.com>
2014-06-16 03:18:30 -0700

    Created Comrades Endpoint
    Created Comrades Endpoint Controller and Model

---
+++ backend/api/controllers/ComradesController.js
@@ -0,0 +1,11 @@
+/**
+ * ComradesController
+ *
+ * @description :: Server-side logic for managing Comrades
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ backend/api/models/Comrades.js
@@ -0,0 +1,29 @@
+/**
+* Comrades.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+    tableName: 'comrades',
+    schema: false,
+    attributes: {
+        userID: {
+            type: 'integer',
+            primaryKey: true,
+            columnName: 'userID'
+        },
+        comradeID: {
+            type: 'integer',
+            columnName: 'comradeID'
+        },
+        comrades: {
+            type: 'integer',
+            defaultsTo: 0,
+            columnName: 'comrades'
+        }
+    }
+};
+
