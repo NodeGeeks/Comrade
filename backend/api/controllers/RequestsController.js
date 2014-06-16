a7a9a642f4e9252d94e8767cba41aac70a1326ff
Aaron Russell <aaron@teknologenie.com>
2014-06-16 03:19:39 -0700

    Created Requests Endpoint
    Created Requests Endpoint Controller and Model, this endpoint handles
    the Invite, Find, Friend Request and misc requests.

---
+++ backend/api/controllers/RequestsController.js
@@ -0,0 +1,11 @@
+/**
+ * RequestsController
+ *
+ * @description :: Server-side logic for managing Requests
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
