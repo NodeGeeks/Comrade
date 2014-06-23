9bcc490be7f1da804fc0db7c8acbd213a78495e7
Aaron Russell <aaron@teknologenie.com>
2014-06-23 15:21:17 -0700

    Read Description
    Now using promises instead of relying on if the value is there or not.
    
    Added Email, Phone Number and Username check to sendComradesRequest

--- backend/api/controllers/ComradesController.js
+++ backend/api/controllers/ComradesController.js
@@ -8,86 +8,139 @@
 
 module.exports = {
     comrades: function(req, res) {
-        if (!req.body.userID || !req.body.accessToken ) {
-            res.serverError('No ID or Token found, are you sure your logged in?')
-        } else {
-            Comrades.find({comradeID: req.body.userID, comrades: 'accepted'}).exec(function afterwards(err, found) {
-                if (found) {
-                    res.json(found);
-                }
-                if (err) {
-                    res.serverError(err);
-                }
-
+        Comrades.find().where({comradeID: req.body.id, comrades: 'approved'})
+            .then(function (found) {
+                return res.json(found);
+            })
+            .fail(function(err) {
+                return res.serverError(err);
             });
-        }
     },
-	sendComradesRequest: function (req, res) {
-        Comrades.create({userID: req.body.userID, comradesID: req.body.comradesID, comrades: 'accepted'}).exec(function aftwards(err, created){
-            if (created) {
-                res.json(created);
-                Comrades.create({userID: req.body.comradesID, comradesID: req.body.userID, comrades: 'pending'}).exec(function aftwards(err, created){
-                    if (created) {
-                        res.json(created);
-
-                    }
-                    if (err) {
-                        res.serverError(err);
-                    }
-                });
-            }
-            if (err) {
-                res.serverError(err);
-            }
+    pendingComradesRequests: function (req, res) {
+        Comrades.find().where({userID: req.body.id, comrades: 'pending'})
+        .then(function (found) {
+            return res.json(found);
+        })
+        .fail(function(err) {
+            return res.serverError(err);
         });
     },
+	sendComradesRequest: function (req, res) {
+
+        var emailRegex = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+        var intRegex = new RegExp(/^\d+$/);
+        var val = req.body.value;
+        if (emailRegex.test(val) == true) {
+            Users.findOne().where({email: val})
+            .then(function (found) {
+                Comrades.create({userID: req.body.id, comradeID: found.id, comrades: 'approved'})
+                .then(function (created){
+                    Comrades.create({userID: found.id, comradeID: req.body.id, comrades: 'pending'})
+                    .then(function (created1){
+                        return res.json(created1);
+                    })
+                    .fail(function(err) {
+                        return res.serverError(err);
+                    });
+                })
+                .fail(function(err) {
+                    return res.serverError(err);
+                });
+            })
+            .fail(function(err){
+                return res.serverError(err);
+            });
+        } else if (intRegex.test(val) == true) {
+            console.log('looks like a phone number to me');
+            Users.findOne()
+                .where({phoneNumber: val})
+                .then(function (found) {
+                    Comrades.create({userID: req.body.id, comradeID: found.id, comrades: 'approved'})
+                        .then(function (created){
+                            Comrades.create({userID: found.id, comradeID: req.body.id, comrades: 'pending'})
+                                .then(function (created1){
+                                    return res.json(created1);
+                                })
+                                .fail(function(err) {
+                                    return res.serverError(err);
+                                });
+                        })
+                        .fail(function(err) {
+                            return res.serverError(err);
+                        });
+                })
+                .fail(function(err){
+                    return res.serverError(err);
+                });
+        } else if (val) {
+            console.log('just a normal username');
+            Users.findOne()
+                .where({comradeUsername: val})
+                .then(function (found) {
+                    Comrades.create({userID: req.body.id, comradeID: found.id, comrades: 'sent'})
+                        .then(function (created){
+                            Comrades.create({userID: found.id, comradeID: req.body.id, comrades: 'pending'})
+                                .then(function (created1){
+                                    return res.json(created1);
+                                })
+                                .fail(function(err) {
+                                    return res.serverError(err);
+                                });
+                        })
+                        .fail(function(err) {
+                            return res.serverError(err);
+                        });
+                })
+                .fail(function(err){
+                    return res.serverError(err);
+                });
+        } else if (!val) {
+            res.notFound('Could not find someone with no data, did you forget to type something perhaps?');
+        }
+
+
+    },
     acceptComradesRequest: function (req, res) {
-        Comrades.update({userID: req.body.userID, comradesID: req.body.comradesID}, {comrades: 'accepted'}).exec(function aftwards(err, update){
-            if (update) {
-                res.json(update);
-            }
-            if (err) {
-                res.serverError(err);
-            }
+        Comrades.update({userID: req.body.userID, comradesID: req.body.comradesID}, {comrades: 'accepted'})
+        .then(function (updated){
+            return res.json(updated);
+        })
+        .fail(function(err) {
+            return res.serverError(err);
         });
     },
     denyComradesRequest: function (req, res) {
-        Comrades.update({userID: req.body.userID, comradesID: req.body.comradesID}, {comrades: 'denied'}).exec(function aftwards(err, update){
-            if (update) {
-                res.json(update);
-                Comrades.update({userID: req.body.comradesID, comradesID: req.body.userID}, {comrades: 'denied'}).exec(function aftwards(err, update){
-                    if (update) {
-                        res.json(update);
-                    }
-                    if (err) {
-                        res.serverError(err);
-                    }
+        Comrades.update({userID: req.body.userID, comradesID: req.body.comradesID}, {comrades: 'denied'})
+        .then(function (updated){
+                Comrades.update({userID: req.body.comradesID, comradesID: req.body.userID}, {comrades: 'denied'})
+                .then(function (updated){
+                    res.json(updated);
+                })
+                .fail(function(err){
+                    res.serverError(err);
                 });
-            }
-            if (err) {
-                res.serverError(err);
-            }
+        })
+        .fail(function(err) {
+            return res.serverError(err);
         });
+
     },
     ignoreComradesRequest: function (req, res) {
-        Comrades.update({userID: req.body.userID, comradesID: req.body.comradesID}, {comrades: 'ignored'}).exec(function aftwards(err, update){
-            if (update) {
-                res.json(update);
-
-            }
-            if (err) {
-                res.serverError(err);
-            }
+        Comrades.update({userID: req.body.userID, comradesID: req.body.comradesID}, {comrades: 'ignored'})
+        .then(function (updated){
+            res.json(updated);
+        })
+        .fail(function(err){
+            res.serverError(err);
         });
     },
     blockComrade: function (req, res) {
-        Comrades.update({userID: req.body.userID, comradesID: req.body.comradesID}, {comrades: 'blocked'}).exec(function aftwards(err, update){
-            if (update) {
-                res.json(update);
-            }
-            if (err) {
-                res.serverError(err);
-            }
+        Comrades.update({userID: req.body.userID, comradesID: req.body.comradesID}, {comrades: 'blocked'})
+        .then(function (updated){
+            res.json(updated);
+        })
+        .fail(function(err){
+            res.serverError(err);
         });
     }
 };
