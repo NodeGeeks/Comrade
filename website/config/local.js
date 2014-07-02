4db9dd665930d554fa089b0a298b4189b86c9d1d
Aaron Russell <aaron@teknologenie.com>
2014-07-01 17:50:12 -0700

    asf
    das

--- website/config/local.js
+++ website/config/local.js
@@ -49,7 +49,7 @@ module.exports = {
   // In production, you'll probably want to change this setting
   // to 80 (http://) or 443 (https://) if you have an SSL certificate
 
-  port: process.env.PORT || 80,
+  port: 80,
 
 
 
@@ -65,6 +65,6 @@ module.exports = {
   // By default, Sails sets its environment using the `NODE_ENV` environment variable.
   // If NODE_ENV is not set, Sails will run in the 'development' environment.
 
-  environment: process.env.NODE_ENV || 'development'
+  environment: process.env.NODE_ENV || 'production'
 
 };
